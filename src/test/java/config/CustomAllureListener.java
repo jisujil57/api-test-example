04262e80e257d7ec7ae73c7cc41a16e1359acd27
v.tanasevskiy <v.tanasevskiy@yaznt.ru>
2024-04-10 00:28:09 +0300

    rework spec

--- src/test/java/config/CustomAllureListener.java
+++ src/test/java/config/CustomAllureListener.java
@@ -5,7 +5,6 @@ import io.qameta.allure.restassured.AllureRestAssured;
 public class CustomAllureListener {
     private static final AllureRestAssured FILTER = new AllureRestAssured();
 
-
     public static AllureRestAssured withCustomTemplates() {
         FILTER.setRequestTemplate("request.ftl");
         FILTER.setResponseTemplate("response.ftl");
